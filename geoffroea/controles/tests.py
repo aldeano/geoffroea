e51aba346de5629b75a639aa005f8384dbe23ae2
Jorge <control1@21p-control1.sag.cl>
2012-09-07 22:54:02 -0300

    test urls buenas

--- geoffroea/controles/tests.py
+++ geoffroea/controles/tests.py
@@ -7,10 +7,20 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+class ControlTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_urls_correctas(self):
+        
+		urls_correctas  = ("/", "/ingreso/")
+        
+		for url in urls_correctas:
+			respuesta = self.client.get(url)
+			self.assertEqual(respuesta.status_code, 200)
+	
+	# def test_urls_incorrectas(self):
+		
+		# urls_incorrectas = ("/sdgf/", "/ingreso/dfgtrh/")
+		
+		# for url in urls_incorrectas:
+			# resp = self.client.page(url)
+			# self.assertEqual(respuesta.status_code, 404)
