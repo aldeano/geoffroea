4e9888b073df5f9bc4083cd24e0cf1f1bea8700d
Jorge <aldea.diaguita@gmail.com>
2012-09-09 21:43:16 -0300

    Definiendo tipos de usuario

--- geoffroea/controles/models.py
+++ geoffroea/controles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TipoUsuario(models.Model):
+
+	tipos_usuarios = (
+		(admin, "Administrador"),
+		(er, "Encargado(a) Regional"),
+		(jf, "Jefe(a) de Turno"),
+		(inspector, "Inspector")
+		)
+
+	usuario = models.OneToOneField(User)
+	tipo = models.CharField(choices=tipos_usuarios)
