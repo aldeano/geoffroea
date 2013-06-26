2d9fdb49c007d81dbc168d0075637d8db9232e7a
Jorge <aldea.diaguita@gmail.com>
2013-06-25 21:29:30 -0400

    Intentando arreglar el usercreationform del admin

--- geoffroea/controles/admin.py
+++ geoffroea/controles/admin.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin, UserCreationForm
 from .models import ControlesFronterizos, Usuario
 
-# class TipoUsuarioAdmin(admin.ModelAdmin):
+class UsuarioCreationForm(UserCreationForm):
+    """
+    Formulario para agregar usuarios en el admin, agrega una función
+    clean-username que no consulta el modelo User
+    """
+    
+    class Meta(UserCreationForm.Meta):
+        model = Usuario
 
-    # list_display = ("usuario", "tipo", "region")
-
-# admin.site.register(TipoUsuario, TipoUsuarioAdmin)
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            Usuario.objects.get(username=username)
+        except Usuario.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['Username_duplicado'])
     
 
-admin.site.register(Usuario, UserAdmin)
+class UsuarioAdmin(admin.ModelAdmin):
+    """
+    Utiliza todas las configuraciones estandar de UserAdmin y cambia el form
+    por defecto para agregar usuario con el form modificado 
+    """
+    add_form = UsuarioCreationForm    
+    
+admin.site.register(Usuario, UsuarioAdmin)
 admin.site.register(ControlesFronterizos)
